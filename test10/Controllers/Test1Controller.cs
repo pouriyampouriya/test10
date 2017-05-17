a4f0f62dd2869ac3407ea1e29bee50a22c50bfbd
pouriyampouriya <Pouriyam70@gmail.com>
2017-05-17 20:21:17 +0430

    newcommit
    this is one commit

---
+++ test10/Controllers/Test1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test10.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
