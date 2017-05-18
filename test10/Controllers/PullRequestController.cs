b5040dc1716f0699e1976ffdba70b901f61561e6
pouriyampouriya <Pouriyam70@gmail.com>
2017-05-18 09:22:59 +0430

    PullRequest
    Pull Request

---
+++ test10/Controllers/PullRequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test10.Controllers
+{
+    public class PullRequestController : Controller
+    {
+        // GET: PullRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
