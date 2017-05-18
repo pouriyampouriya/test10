d14cd772f6eb28caf4c598a57d9a22414dda49e0
pouriyampouriya <Pouriyam70@gmail.com>
2017-05-18 10:15:41 +0430

    Revert "Revert "NewTest3""
    This reverts commit fba8f0ec165437ae0d2774474274725d183d9886.

---
+++ test10/Controllers/NewTest3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test10.Controllers
+{
+    public class NewTest3Controller : Controller
+    {
+        // GET: NewTest3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
